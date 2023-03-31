035bdaa5794f7b1e17c52fabca84601bc8f4fe9d
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-03-31 09:42:03 +0530

    Add files via upload

---
+++ Twist Taste/Twist Node.js
@@ -0,0 +1,6 @@
+function f1() {
+    let c1 = document.getElementById('input here');
+    if(c1==' ') {
+        alert("Blank Input");
+    }
+}
---
+++ Twist Taste/node.js
@@ -0,0 +1,21 @@
+function sethere() {
+    document.getElementsByClassName('set', f1 = ()=> {
+        const s1 = document.getElementById('head1');
+        const s2 = document.getElementById('head2');
+        const s3 = document.getElementById('head3');
+        const s4 = document.getElementById('head4');
+    });
+}
+
+myhead = ()=> {
+    const p1 = document.getElementById('container1',f2 = ()=> {
+        const menu = document.querySelector('#menu1',function g() {
+            const S1 = document.getElementById('set2');
+            S1.style.boxShadow = "2px 2px 4px darkgreen";
+        });
+    });
+}
+
+const ordernow = document.getElementById('section2', r1 = ()=> {
+    menu.style.getElementsByClassName;
+});
